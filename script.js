4c618566851d186206d5ea1d12f2ed9d594f1bf3
Gino Fabrizio <51064212+SirTortuga333@users.noreply.github.com>
2025-02-06 22:30:05 -0300

    Fixed bug on claiming claimed cells Added win and tie checks Added sanitization on player input for console even though is not needed

--- script.js
+++ script.js
@@ -9,16 +9,14 @@ const gameBoard = (function(){
     for (let i = 0; i < 9; i++) {
         board.push(cell());
     }
-
-    console.log(board)
     
     function markCell(cell, player){
 
         // Invalid move on already marked cell
-        if(board[cell].getMark() !== ' ') return;
+        if(board[cell].getMark() !== ' ') return true;
 
         board[cell].getClaimed(player);
-
+        return false;
     }
 
     // Print board to console
@@ -28,7 +26,11 @@ const gameBoard = (function(){
         console.log(board[6].getMark(),'|', board[7].getMark(),'|', board[8].getMark());
     }
     
-    return {printBoard, markCell}
+    function getBoard(){
+        return board;
+    }
+
+    return {printBoard, markCell, getBoard}
 })();
 
 function cell(){
@@ -57,6 +59,37 @@ const gameController = (function(playerOneName = "Player One", playerTwoName = "
         }
     ]
 
+    // Save all winning combinations
+    const winningCombinations = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    function checkWin(){
+        // We loop all combinations, deconstruct them and check if the active player did a winning move
+        for (let index = 0; index < winningCombinations.length; index++) {
+            const [a,b,c] = winningCombinations[index];
+            if (
+                gameBoard.getBoard()[a].getMark() === getActivePlayer().mark &&
+                gameBoard.getBoard()[b].getMark() === getActivePlayer().mark &&
+                gameBoard.getBoard()[c].getMark() === getActivePlayer().mark
+            ) return true
+        }
+        return false
+    
+    }
+
+    // we loop through all the board to check every tile is mark
+    function checkTie(){
+        return gameBoard.getBoard().every((cell) => cell.getMark() !== ' ');
+    }
+
     let activePlayer = players[0];
     
     function switchPlayerTurn(){
@@ -75,12 +108,30 @@ const gameController = (function(playerOneName = "Player One", playerTwoName = "
     function playRound(cell){
 
         // Logic for sanitize user's input (number and 0-8)
+        if(typeof(cell) !== 'number' || (cell < 0 && cell > 8)){
+            console.log(`Input must be a number between 0 - 8.`);
+            return;
+        }
 
         console.log(`${getActivePlayer().name} claimed cell ${cell}`);
-        gameBoard.markCell(cell, getActivePlayer().mark);
-
+        // Loop to check if player tries to claim a claimed cell
+        let invalidMove = gameBoard.markCell(cell, getActivePlayer().mark);
+        while (invalidMove) {
+            console.log(`${cell} is already claimed, please select another cell.`);
+            return false;
+        }
 
         // Winning logic
+        if(checkWin()){
+            printNewRound();
+            console.log(`${getActivePlayer().name} has won! Game Over.`);
+            return false;
+        }
+        if(checkTie()){
+            printNewRound();
+            console.log('The board is full, the game is a tie! Nobody Wins.');
+            return false;
+        }
 
         switchPlayerTurn();
         printNewRound();
